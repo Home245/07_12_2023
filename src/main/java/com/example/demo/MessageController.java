60d4daa1f3e0682c18c655d14d5930ac0afd6da3
Home245 <148992841+Home245@users.noreply.github.com>
2023-12-12 00:14:23 +0300

    Update ApiController.java

--- src/main/java/com/example/demo/MessageController.java
+++ src/main/java/com/example/demo/MessageController.java
@@ -1,135 +1,95 @@
 package com.example.demo;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 @RestController
-public class MessageController {
-    private final List<String> messages = new ArrayList<>();
-    private final List<User> users = new ArrayList<>();
+public class ApiController {
+    private final List<String> topics = new ArrayList<>();
+    private final List<List<String>> messagesInTopic = new ArrayList<>();
 
-    //curl -X GET http://localhost:8080/messages
-    @GetMapping("messages")
-    public ResponseEntity<List<String>> getMessages() {
-        return ResponseEntity.ok(messages);
-    }
-
-    //curl -X GET http://localhost:8080/messages/from/0
-    @GetMapping("messages/from/{index}")
-    public ResponseEntity<List<String>> getMessages(@PathVariable("index") Integer index) {
-        List<String> answermessages = new ArrayList<>();
-        for(int i = index; i < messages.size(); i++){
-            answermessages.add(messages.get(i));
-        }
-        return ResponseEntity.ok(answermessages);
-    }
-
-    //curl -X POST -H "Content-Type: text/plain" http://localhost:8080/messages -d "Majula"
-    @PostMapping("messages")
-    public ResponseEntity<Void> addMessage(@RequestBody String text) {
-        messages.add(text);
+    //3
+    //curl -X POST -H "Content-Type: text/plain" http://localhost:8080/topics -d "Majula"
+    @PostMapping("topics")
+    public ResponseEntity<Void> addTopic(@RequestBody String text) {
+        topics.add(text);
+        List<String> thislist = new ArrayList<>();
+        messagesInTopic.add(thislist);
         return ResponseEntity.accepted().build();
     }
 
-    //curl -X GET http://localhost:8080/messages/2
-    @GetMapping("messages/{index}")
-    public ResponseEntity<String> getMessage(@PathVariable("index") Integer
-                                                     index) {
-        return ResponseEntity.ok(messages.get(index));
-    }
-
-    //curl -X DELETE http://localhost:8080/messages/1
-    @DeleteMapping("messages/{index}")
-    public ResponseEntity<Void> deleteText(@PathVariable("index") Integer
-                                                   index) {
-        messages.remove((int) index);
+    //curl -X DELETE http://localhost:8080/topics/1
+    @DeleteMapping("topics/{index}")
+    public ResponseEntity<Void> deleteTopic(@PathVariable("index") Integer index) {
+        topics.remove((int) index);
         return ResponseEntity.noContent().build();
     }
 
-    //curl -X PUT -H "Content-Type: text/plain" http://localhost:8080/messages/1 -d "Pinwheel"
-    @PutMapping("messages/{index}")
+    //curl -X GET http://localhost:8080/topics
+    @GetMapping("topics")
+    public ResponseEntity<List<String>> getMessages() {
+        return ResponseEntity.ok(topics);
+    }
+
+    //curl -X PUT -H "Content-Type: text/plain" http://localhost:8080/topics/1 -d "Pinwheel"
+    @PutMapping("topics/{index}")
     public ResponseEntity<Void> updateMessage(
             @PathVariable("index") Integer i,
             @RequestBody String message) {
-        messages.remove((int) i);
-        messages.add(i, message);
+        topics.remove((int) i);
+        topics.add(i, message);
         return ResponseEntity.accepted().build();
     }
 
-    //curl -X GET http://localhost:8080/messages/search/ababa
-    @GetMapping("messages/search/{text}")
-    public ResponseEntity<Integer> getMessage(@PathVariable("text") String text) {
-        for(int i = 0; i < messages.size(); i++){
-            if(messages.get(i).contains(text)){
-                return ResponseEntity.ok(i+1);
-            }
-        }
-        return ResponseEntity.ok(-1);
+    //curl -X GET http://localhost:8080/topics/count
+    @GetMapping("topics/count")
+    public ResponseEntity<Integer> getTopic(){
+        return ResponseEntity.ok(topics.size());
     }
 
-    //curl -X GET http://localhost:8080/messages/count
-    @GetMapping("messages/count")
-    public ResponseEntity<Integer> getMessage(){
-        return ResponseEntity.ok(messages.size());
-    }
-
-    //curl -X POST -H "Content-Type: text/plain" http://localhost:8080/messages/4/create
-    @PostMapping("messages/{index}/create")
-    public ResponseEntity<Void> addMessage(@PathVariable("index") Integer index) {
-        messages.add(index, "");
-        return ResponseEntity.accepted().build();
-    }
-
-    //curl -X DELETE http://localhost:8080/messages/search/Nashandra
-    @DeleteMapping("messages/search/{text}")
-    public ResponseEntity<Void> deleteText(@PathVariable("text") String text) {
-        int temp = 0;
-        while (temp != messages.size()){
-            if(messages.get(temp).contains(text)){
-                messages.remove(temp);
-                temp = 0;
-            } else{
-                temp++;
-            }
-        }
+    //curl -X DELETE http://localhost:8080/alltopics
+    @DeleteMapping("alltopics")
+    public ResponseEntity<Void> deleteTopics() {
+        topics.clear();
+        messagesInTopic.clear();
         return ResponseEntity.noContent().build();
     }
 
-    //curl -X POST -H "Content-Type: application/json" localhost:8080/users -d "{\"name\":\"Vendrik\",\"age\":\"200\"}"
-    @PostMapping("users")
-    public ResponseEntity<Void> addUser(@RequestBody User user) {
-        users.add(user);
+    //curl -X GET http://localhost:8080/topics/2
+    @GetMapping("topics/{index}")
+    public ResponseEntity<String> getTopic(@PathVariable("index") Integer index) {
+        return ResponseEntity.ok(topics.get(index));
+    }
+
+    //4
+    //curl -X POST -H "Content-Type: text/plain" http://localhost:8080/topics/1/messages -d "Majula"
+    @PostMapping("topics/{index}/messages")
+    public ResponseEntity<Void> addTopic(@PathVariable("index") Integer index, @RequestBody String text) {
+        messagesInTopic.get(index).add(messagesInTopic.get(index).size(), text);
         return ResponseEntity.accepted().build();
     }
 
-    //curl -X GET localhost:8080/users
-    @GetMapping("users")
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(users);
-    }
-
-    //curl -X GET localhost:8080/users/1
-    @GetMapping("users/{index}")
-    public ResponseEntity<User> getUser(@PathVariable("index") Integer index) {
-        return ResponseEntity.ok(users.get(index));
-    }
-
-    //curl -X DELETE http://localhost:8080/users/2
-    @DeleteMapping("users/{index}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("index") Integer index) {
-        users.remove((int)index);
+    //curl -X DELETE http://localhost:8080/topics/1/messages/1
+    @DeleteMapping("topics/{index}/messages/{number}")
+    public ResponseEntity<Void> deleteTopic(@PathVariable("index") Integer index, @PathVariable("index") Integer number) {
+        messagesInTopic.get((int)index).remove((int) number);
         return ResponseEntity.noContent().build();
     }
 
-    //curl -X PUT -H "Content-Type: application/json" http://localhost:8080/users/change/age/1 -d "10"
-    @PutMapping("users/change/age/{index}")
-    public ResponseEntity<Void> updateAge(@PathVariable("index") Integer index, @RequestBody Integer age) {
-        User changeableUser = users.get(index);
-        changeableUser.setAge(age);
-        users.remove((int) index);
-        users.add(index, changeableUser);
+    //curl -X PUT -H "Content-Type: text/plain" http://localhost:8080/topics/1/messages/1 -d "Pinwheel"
+    @PutMapping("topics/{index}/messages/{number}")
+    public ResponseEntity<Void> updateMessage(@PathVariable("index") Integer index, @PathVariable("index") Integer number, @RequestBody String message) {
+        messagesInTopic.get((int)index).remove((int) number);
+        messagesInTopic.get((int) index).add(message);
         return ResponseEntity.accepted().build();
+    }
+
+    //curl -X GET http://localhost:8080/topics/1/messages
+    @GetMapping("topics/{index}/messages")
+    public ResponseEntity<List<String>> getMessages(@PathVariable("index") Integer index) {
+        return ResponseEntity.ok(messagesInTopic.get((int)index));
     }
 }
